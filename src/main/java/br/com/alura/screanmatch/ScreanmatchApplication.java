1929c0963443472be698c20ce53a5ec915f35493
Luiz Lincoln <lincolndovale07@gmail.com>
2024-05-18 19:31:21 -0300

    Primeiro projeto Spring

---
+++ src/main/java/br/com/alura/screanmatch/ScreanmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.alura.screanmatch;
+
+import br.com.alura.screanmatch.model.DadosSerie;
+import br.com.alura.screanmatch.service.ConsumoApi;
+import br.com.alura.screanmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreanmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreanmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+//		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
